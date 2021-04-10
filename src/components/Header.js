aa0ff1e9d50daa4f9d21f2acb2f6071ea140fbe6
Oskar Info <racso1607@gmail.com>
2021-04-10 11:12:04 -0500

    fix console some  errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
 import '../assets/styles/components/Header.scss'
 import logo from '../assets/images/posteoLogoMobile.png';
 import logoDesktop from '../assets/images/posteoLogoDesktop.png';
@@ -7,38 +7,36 @@ import Form from '../components/FormToPost'
 
 
 const Header = () => {
+    function handleClick(e) {
+        e.preventDefault();
+        console.log('The link was clicked.');
+        return (
+            <Form />
+        )
+    }
     return (
         <header>
-            <div class="logo__header">
+            <div className="logo__header">
                 <picture>
-                    <source media="(min-width: 700px)" srcset={logoDesktop}/>
+                    <source media="(min-width: 700px)" srcSet={logoDesktop}/>
                     <img src={logo} alt="Logo Posteo"/>
                 </picture>
                 <img src={logoText} alt="" className="logo__text"></img>
             </div>
-            {/* prueba dropdown
-            <div class="dropdownPrueba">
-                    <label for="prueba">click</label>
-                    <input type="checkbox" id="prueba"></input>
-                    <div class="dropdown-content">
-                        <p>Hello World!</p>
-                    </div>
-                </div>
-                Cierra prueba dropdown */}
             <div className="nav__container">
-                <button className="button__post">POST</button>
+                <button className="button__post" onClick={handleClick}>POST</button>
                 <Menu></Menu>
             </div>
-            <Form/>
+            {/* <Form/> */}
         </header>
     );
 };
 
-function Menu(props){
+function Menu(){
 
     return (
-        <div class="dropdown">
-            <label for="prueba" className="menu__button">Opciones</label>
+        <div className="dropdown">
+            <label htmlFor="prueba" className="menu__button">Opciones</label>
             <input type="checkbox" id="prueba" ></input>
             <div className="dropdown-content">
                 <a href="#" className="menu-item">Mi perfil</a><br/>
